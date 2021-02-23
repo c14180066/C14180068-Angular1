995655c29959ac8b40a8348988605628590974ef
hanstjandra68 <c14180068@john.petra.ac.id>
2021-02-23 18:34:14 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  count = 0;
+  user = "user 1";
+  a = 0;
+  b = 0;
+
+  arr_name: string[][] = [
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"]
+  ];
+
+  EventKlik() {
+    if (this.count % 2 == 0) {
+      this.user = "user 2";
+    } else {
+      this.user = "user 1";
+    }
+    this.count = this.count + 1;
+  }
 }
